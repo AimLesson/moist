1a4db002c947e3ee6f380bce8581b7616d5b2a81
AimLesson <113356516+AimLesson@users.noreply.github.com>
2024-07-30 22:42:09 +0700

    update

--- resources/views/karyawan.blade.php
+++ resources/views/karyawan.blade.php
@@ -7,35 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Nama
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Email
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $orders = \App\Models\User::get();
-                            @endphp
-                            @foreach ($orders as $order)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
-                                <td class="px-6 py-4">
-                                    {{ $order->nama }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $order->email }}
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nama
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Email
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $orders = \App\Models\User::where('role', 'admin')->get();
+                        @endphp
+                        @foreach ($orders as $order)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
+                            <td class="px-6 py-4">
+                                {{ $order->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $order->email }}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </x-app-layout>
